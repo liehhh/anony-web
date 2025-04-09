6c611e3bf163b24082794c410d0fc6ae00608e56
unseenmeli <unseenmeli@gmail.com>
2025-04-09 20:47:50 +0400

    Front. Meow

--- anon_web/confess/models.py
+++ anon_web/confess/models.py
@@ -9,3 +9,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment on {self.post.title}'
--- anon_web/confess/views.py
+++ anon_web/confess/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .forms import PostForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm, CommentForm
 from .models import Post
 
 
@@ -9,9 +9,25 @@ def home(request):
     }
     return render(request, 'confess/home.html', context)
 
+
 def story(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    comments = post.comments.all().order_by('-date_posted')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('confess-story', pk=post.id)
+    else:
+        form = CommentForm()
+
     context = {
-        'item': Post.objects.get(id=pk)
+        'item': post,
+        'comments': comments,
+        'form': form
     }
     return render(request, 'confess/story.html', context)
 
